599238713c34a6b92d114a6a39c3cb19262b2b17
Luccas99 <55700307+Luccas99@users.noreply.github.com>
2019-10-07 16:59:14 -0300

    Add files via upload

---
+++ phpex.php
@@ -0,0 +1,19 @@
+<?php
+
+$anosdevida = 71;
+
+if ($anosdevida >= 15 && $anosdevida < 60) {
+
+	echo "Você é um aborrescente!!!";
+}
+
+elseif ($anosdevida > 60) {
+
+	echo "Está com o pé na cova!!!";
+}
+
+else {
+	echo " Você ainda é um pivete!!!";
+}
+
+?>
